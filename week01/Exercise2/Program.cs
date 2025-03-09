79f536ae3c2992a076f37fb8406fa04cbb1e81e1
Jmanx7 <78266433+Jmanx7@users.noreply.github.com>
2025-03-09 11:41:52 -0700

    Completed grades assignment.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What's your grade percentage? ");
+        string gradeinput = Console.ReadLine();
+        int gradeno = int.Parse(gradeinput);
+
+        string gradelet = "";
+
+        if (gradeno >= 90)
+        {
+            gradelet = "A";
+        }  
+
+        else if (gradeno >= 80)
+        {
+            gradelet = "B";
+        } 
+
+        else if (gradeno >= 70)
+        {
+            gradelet = "C";
+        } 
+
+        else if (gradeno >= 60)
+        {
+            gradelet = "D";
+        } 
+
+        else if (gradeno <60)
+        {
+            gradelet = "F";
+        }
+
+        Console.Write($"Your letter grade is {gradelet}. ");
+
+        if (gradeno >= 70)
+        {
+            Console.Write("Congratulations on passing the course!");
+        }
+        else if (gradeno <70)
+        {
+            Console.Write("Well, maybe you'll pass next time!");
+        }
     }
 }

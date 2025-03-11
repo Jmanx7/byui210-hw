74a8cc8eb14afd3bf344481f7f9c3a4ec4dc6ef0
Jmanx7 <78266433+Jmanx7@users.noreply.github.com>
2025-03-10 23:24:09 -0700

    Completed number list assignment.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int numinput = -1;
+
+        Console.WriteLine("Please input a list of numbers. When done, input a 0. ");
+
+        do
+        {
+            Console.Write("Input a number. ");
+            numinput = int.Parse(Console.ReadLine());
+            if (numinput != 0)
+            {
+                numbers.Add(numinput);
+            }
+
+        } while (numinput != 0);
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total = total + number;
+        }
+
+        int numberofnumbers = numbers.Count;
+        int numbermean = total / numberofnumbers;
+
+        int highest = -1;
+        foreach (int number in numbers)
+        {
+            if (number > highest)
+            {
+                highest = number;
+            }
+        }
+
+        Console.Write($"The sum is {total}. ");
+        Console.Write($"The average is {numbermean}. ");
+        Console.Write($"The largest number is {highest}. ");
+
     }
 }

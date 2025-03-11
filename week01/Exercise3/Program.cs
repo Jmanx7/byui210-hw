a3306fe2f681e87f4d6cfa3eb0b98112f608e568
Jmanx7 <78266433+Jmanx7@users.noreply.github.com>
2025-03-10 22:55:32 -0700

    Completed guessing game assignment.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicno = randomGenerator.Next(1,10);
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What's your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicno)
+            {
+                Console.Write("Lower! ");
+            }
+
+            else if (guess < magicno)
+            {
+                Console.Write("Higher! ");
+            }
+        } while (guess != magicno); 
+
+        Console.Write("You got it! ");
     }
 }

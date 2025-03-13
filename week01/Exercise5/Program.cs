52809a188da5a261a2e1e75d970ac549f7797605
Jmanx7 <78266433+Jmanx7@users.noreply.github.com>
2025-03-13 11:57:58 -0700

    Completed functions exercise.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program! ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        int numinput = -1;
+        Console.Write("Please enter your favorite number: ");
+        numinput = int.Parse(Console.ReadLine());
+
+        return numinput;
+    }
+
+    static int SquareNumber(int numinput)
+    {
+        int squareresult = numinput * numinput;
+        return squareresult;
+    }
+
+    static void DisplayResult(string name, int squareresult)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareresult}.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string username = PromptUserName();
+        int usernumber = PromptUserNumber();
+
+        int squarenumber = SquareNumber(usernumber);
+
+        DisplayResult(username, squarenumber);
     }
 }
